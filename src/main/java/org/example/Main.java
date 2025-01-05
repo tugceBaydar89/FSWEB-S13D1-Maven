65a7371c6dfef412ab4bf7216d7cd4eb105d4aa5
tugceBaydar89 <tugce.baydar1@gmail.com>
2025-01-05 20:55:04 +0300

    tug

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,44 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock <0 || clock > 23 || !isBarking) {
+            return false;
+
+        }
+        return clock >=20 || clock <8;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       if ((firstAge>13 && firstAge<=19) || (secondAge>13 && secondAge<=19) || (thirdAge>13 && thirdAge<=19)) {
+           return true;
+       } else {
+           return false;
+       }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     int lowestTemp = 25;
+     int highTemp = isSummer? 45 : 35;
+     if (temp <highTemp && temp>lowestTemp) {
+         return true;
+     }else {
+         return false;
+     }
     }
 
     public static double area(double width, double height) {
-       
+       if (width<0 || height<0) {
+           return -1;
+       }else {
+           return width*height;
+       }
     }
 
     public static double area(double radius) {
-       
+       if (radius<0) {
+           return -1;
+       }else {
+           return Math.PI*Math.pow(radius,2);
+       }
     }
 }
